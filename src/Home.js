1f854e2ce3fb47f1e4acce3ec3fef48ac7a6d537
Vignesh <joshi.vignesh@gmail.com>
2024-05-02 21:14:33 +0530

    Initial and complete commit for fetching University details

---
+++ src/Home.js
@@ -0,0 +1,17 @@
+import React, { Component} from 'react';
+import Navbar from './Navbar.js';
+import MainPage from './MainPage';
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <Navbar />
+                <MainPage />
+            </div>
+           
+            );
+    }
+}
+
+export default Home;
